3778dfd1c298e15ba20eba7749a1cde82fe88f9d
Elina0126 <59711107+Elina0126@users.noreply.github.com>
2023-02-22 10:50:07 +0100

    Update mf_df.R

--- mf_df.R
+++ mf_df.R
@@ -6,11 +6,11 @@ library('missForest')
 library('randomForest')
 library("FactoMineR")
 library("factoextra")
-df <- read.csv(file = '/home/elina/Documents/thesis/df_o_fill.csv')
+df <- read.csv(file = 'dataframe/df_o_fill.csv')
 for (i in 1:55){df[,i] <- as.factor(df[,i])}
 df[df == 'NaN'] <- NA
 df_im <- missForest(df)
 df <- df_im$ximp
 df <- type.convert(df)
 summary(df)
-write.csv(df,"/home/elina/Documents/thesis/missForest_df_ofill.csv", row.names = FALSE)
+write.csv(df,"dataframe/missForest_df_ofill.csv", row.names = FALSE)
